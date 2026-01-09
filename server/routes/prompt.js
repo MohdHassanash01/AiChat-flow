388784d784bbfff3a61b751d8bf272b269c60a06
Mohd Hassan Ashraf <125294445+MohdHassanash01@users.noreply.github.com>
2026-01-09 16:14:59 +0530

    add http-referer-url

--- server/routes/prompt.js
+++ server/routes/prompt.js
@@ -36,7 +36,7 @@ promptRouter.post("/prompt",async function(req,res){
         headers: {
           Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
           "Content-Type": "application/json",
-          "HTTP-Referer": "http://localhost:3000",
+          "HTTP-Referer": `${process.env.HTTP-Referer_URL}`,
           "X-Title": "Future Blick",
         },
       }
